c15f83f21c796f243d9dfd4afe3f1911423b79d0
Nabilfadilah <mohammadfadilah00@gmail.com>
2025-08-24 10:56:16 +0700

    Router sederhana dengan path_info

--- public/index.php
+++ public/index.php
@@ -1,3 +1,26 @@
 <?php
 
-echo "Hello Nabil Ganteng";
+// require_once __DIR__ . '/../vendor/autoload.php';
+
+$path = "/index";
+
+// mendapat dari path info
+if (isset($_SERVER['PATH_INFO'])) {
+    $path = $_SERVER['PATH_INFO'];
+}
+
+// lalu ambil path nya, misal /login, /register, dll
+require __DIR__ . '/../app/view' . $path . '.php';
+// use ProgrammerZamanNow\Belajar\PHP\MVC\App\Router;
+// use ProgrammerZamanNow\Belajar\PHP\MVC\Controller\HomeController;
+// use ProgrammerZamanNow\Belajar\PHP\MVC\Controller\ProductController;
+// use ProgrammerZamanNow\Belajar\PHP\MVC\Middleware\AuthMiddleware;
+
+// Router::add('GET', '/products/([0-9a-zA-Z]*)/categories/([0-9a-zA-Z]*)', ProductController::class, 'categories');
+
+// Router::add('GET', '/', HomeController::class, 'index');
+// Router::add('GET', '/hello', HomeController::class, 'hello', [AuthMiddleware::class]);
+// Router::add('GET', '/world', HomeController::class, 'world', [AuthMiddleware::class]);
+// Router::add('GET', '/about', HomeController::class, 'about');
+
+// Router::run();
